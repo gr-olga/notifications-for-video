f100d1bd6be9ad93b16da2e42fddf792db6892ce
Olga Panfilova <gr-olga@ya.ru>
2023-11-17 11:46:30 +0100

    added video and data

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,10 @@ import React from "react";
  const App = () => {
     return (
         <div>
-            <h1>Hello, 555 TypeScript React!</h1>
+            <h1>Hello, let see the video</h1>
+            <video controls>
+                <source src={`http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4`} type="video/webm" />
+            </video>
         </div>
     );
 };
