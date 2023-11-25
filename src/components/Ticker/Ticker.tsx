e711eca4eda0ccc5b80975a5d90a372f28993ad0
Olga Panfilova <gr-olga@ya.ru>
2023-11-25 23:23:22 +0100

    loop tickers

--- src/components/Ticker/Ticker.tsx
+++ src/components/Ticker/Ticker.tsx
@@ -4,18 +4,29 @@ import styles from "./ticker.module.scss";
 
 export default function Ticker(): React.ReactElement {
     const [tickers, setTickers] = useState<TickerItem[]>([]);
+    const [current, setCurrent] = useState<number>(0);
 
     useEffect((): void => {
-        fetch('http://localhost:8081/video-data/tickers')
-            .then((response: Response) => response.json())
-            .then((data: TickerItem[]): void => setTickers(data))
+        getTickers().then(() => loopTickers())
     }, []);
+
+    async function getTickers(): Promise<void> {
+        let response: Response = await fetch('http://localhost:8081/video-data/tickers');
+        setTickers(await response.json());
+    }
+
+    function loopTickers(): void {
+        setInterval((): void => {
+            const val: number = current === tickers.length - 1 ? 0 : current + 1;
+            setCurrent(val);
+        }, 1000);
+    }
 
     return (
         <div className={styles.ticker}>
-            {tickers.map((item: TickerItem) => {
-                return <div className={styles.items}><div className={styles.item}>{item.body}</div></div>;
-            })}
+            <div className={styles.items}>
+                { !!tickers[current] && <div className={styles.item}>{tickers[current].body}</div> }
+            </div>
         </div>
     );
 };
