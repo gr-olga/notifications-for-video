4f1abe9ef36abe023af5404689ffc84f6b3f9bb2
Olga Panfilova <gr-olga@ya.ru>
2023-11-25 23:39:43 +0100

    infinite ticker

--- src/components/Ticker/Ticker.tsx
+++ src/components/Ticker/Ticker.tsx
@@ -4,10 +4,9 @@ import styles from "./ticker.module.scss";
 
 export default function Ticker(): React.ReactElement {
     const [tickers, setTickers] = useState<TickerItem[]>([]);
-    const [current, setCurrent] = useState<number>(0);
 
     useEffect((): void => {
-        getTickers().then(() => loopTickers())
+        getTickers()
     }, []);
 
     async function getTickers(): Promise<void> {
@@ -15,17 +14,14 @@ export default function Ticker(): React.ReactElement {
         setTickers(await response.json());
     }
 
-    function loopTickers(): void {
-        setInterval((): void => {
-            const val: number = current === tickers.length - 1 ? 0 : current + 1;
-            setCurrent(val);
-        }, 1000);
+    function getTickersStr(tickers: TickerItem[]): string {
+        return tickers.reduce((p: string, c: TickerItem): string => p + ' ' + c.body, '')
     }
 
     return (
         <div className={styles.ticker}>
             <div className={styles.items}>
-                { !!tickers[current] && <div className={styles.item}>{tickers[current].body}</div> }
+                <div className={styles.item}>{getTickersStr(tickers)}</div>
             </div>
         </div>
     );
