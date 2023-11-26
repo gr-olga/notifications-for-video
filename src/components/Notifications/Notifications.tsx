3d181463f8adab9110c1da333988ad3cd05596ff
Olga Panfilova <gr-olga@ya.ru>
2023-11-26 01:07:27 +0100

    fixes

--- src/components/Notifications/Notifications.tsx
+++ src/components/Notifications/Notifications.tsx
@@ -10,7 +10,9 @@ type NotificationsProps = {
 
 export default function Notifications({time, eventsList}: NotificationsProps) {
     const [eventsObj, setEventsObj] = useState<Record<string, SportEvent>>({});
-    const [notificarionsList, setNotificarionsList] = useState<any[]>([]);
+    const [notificarionsList, setNotificationsList] = useState<any[]>([]);
+
+    const notificationRemoveDelay: number = 10000;
 
     useEffect((): void => {
         const obj: Record<string, SportEvent> = {};
@@ -19,8 +21,20 @@ export default function Notifications({time, eventsList}: NotificationsProps) {
     }, [eventsList])
 
     useEffect((): void => {
-        if (eventsObj[time]) setNotificarionsList([...notificarionsList, <NotificationCard item={eventsObj[time]} /> as any ])
+        if (eventsObj[time]) {
+            const id: string = eventsObj[time].id;
+            setNotificationsList([...notificarionsList, <NotificationCard item={eventsObj[time]} key={eventsObj[time].id} /> as any])
+            removeNotification(id, notificationRemoveDelay);
+        }
     }, [time])
+
+    function removeNotification(id: string, delay: number): void {
+        setTimeout(() => {
+            //TODO: This doesn't work fully, cause it removes all notifications
+            // setNotificationsList(notificarionsList.filter((item: SportEvent): boolean => item.id !== id));
+            console.log('should remove a notification with id', id);
+        }, delay)
+    }
 
     return (
         <div className={styles.notifications}>
